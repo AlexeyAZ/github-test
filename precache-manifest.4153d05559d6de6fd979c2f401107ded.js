aed672fd9f630477f4f29832450e7d582cdd2b5b
Алексей Андреевич <teos.nl@gmail.com>
2020-09-18 23:08:08 +0300

    Updates

---
+++ precache-manifest.4153d05559d6de6fd979c2f401107ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467bed2ff0e2208fdb1cb6891fe95fe1",
+    "url": "/%7Bgithub-test%7D/index.html"
+  },
+  {
+    "revision": "d006906a8e4cd7925a32",
+    "url": "/%7Bgithub-test%7D/static/css/main.253d8181.chunk.css"
+  },
+  {
+    "revision": "2c2325b6b1d32748aeb3",
+    "url": "/%7Bgithub-test%7D/static/js/2.8e7d606b.chunk.js"
+  },
+  {
+    "revision": "b1f93f4ce18f378bf01e6dbccea3cac2",
+    "url": "/%7Bgithub-test%7D/static/js/2.8e7d606b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d006906a8e4cd7925a32",
+    "url": "/%7Bgithub-test%7D/static/js/main.bee0f4b3.chunk.js"
+  },
+  {
+    "revision": "da140ecf7792d45ccfdd",
+    "url": "/%7Bgithub-test%7D/static/js/runtime-main.0abdbc4d.js"
+  }
+]);
