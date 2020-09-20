2ba3b0c2d8bae8b48ab96d4eddbf09d9ede3cdc3
Алексей Андреевич <teos.nl@gmail.com>
2020-09-20 21:33:10 +0300

    Updates

--- precache-manifest.d48dbd5672174b3fc86b0d19f453f32c.js
+++ precache-manifest.d48dbd5672174b3fc86b0d19f453f32c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "45fa3fdba304cda241e5e49670a09c6c",
+    "revision": "a55280a9632e9e7005faacc7321fff90",
     "url": "/github-test/index.html"
   },
   {
-    "revision": "c41a1819af8f791d59c1",
+    "revision": "6a9eeb859950005c4b1d",
     "url": "/github-test/static/css/main.253d8181.chunk.css"
   },
   {
-    "revision": "2c2325b6b1d32748aeb3",
-    "url": "/github-test/static/js/2.8e7d606b.chunk.js"
+    "revision": "5b23286ca2064dec3966",
+    "url": "/github-test/static/js/2.ac85c819.chunk.js"
   },
   {
     "revision": "b1f93f4ce18f378bf01e6dbccea3cac2",
-    "url": "/github-test/static/js/2.8e7d606b.chunk.js.LICENSE.txt"
+    "url": "/github-test/static/js/2.ac85c819.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c41a1819af8f791d59c1",
-    "url": "/github-test/static/js/main.634b2a4f.chunk.js"
+    "revision": "6a9eeb859950005c4b1d",
+    "url": "/github-test/static/js/main.45ea543e.chunk.js"
   },
   {
     "revision": "992330d26a6062174563",
